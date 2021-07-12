627a472c79426e8cc54859422fba687a90b08ecb
pauloCheloni <paulocheloni@gmail.com>
2021-07-12 17:37:44 -0300

    chore: add node-sass, add bootstrap
    refactor: modified presentation structure

--- src/core/irrfCalc.spec.js
+++ src/core/irrfCalc.spec.js
@@ -17,3 +17,5 @@ test('should return valid irrf discount', () => {
 
 
 })
+
+
---
+++ src/presentation/components/Table/Table.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Table() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <th>um</th>
+          <th>dois</th>
+        </thead>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
